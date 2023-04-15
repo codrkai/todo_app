e9b3a82076161dfdfc6f052f78979b864b165b70
codrkai <codrkai@gmail.com>
2023-04-15 12:19:26 -0700

    initial

--- pages/index.js
+++ pages/index.js
@@ -1,124 +1,116 @@
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import { useState } from 'react'
+import { format } from 'date-fns'
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const [todo, setTodo] = useState([])
+  const [newTodo, setNewTodo] = useState('')
+
+  const getRandomNumber = () => {
+    return Math.floor(Math.random() * 9999)
+  }
+
+  const handleKeyUp = (key) => {
+    if (key === 'Enter' && newTodo) {
+      const randomNumber = getRandomNumber()
+      
+      const newItem = {
+        id: `item-${randomNumber}`,
+        content: newTodo
+      }
+
+      setTodo(todo.concat(newItem))
+
+      setNewTodo('')
+    }
+  }
+
+  const handleDelete = (id) => {
+    if (id > -1) {
+      setTodo(todo.slice(0, id).concat(todo.slice(id+1)))
+    }
+  }
+
+  const reorder = (list, startIndex, endIndex) => {
+    const result = Array.from(list)
+    const [removed] = result.splice(startIndex, 1)
+    result.splice(endIndex, 0, removed)
+    return result
+  }
+
+  const handleOnDragEnd = (result) => {
+    const { destination, source } = result
+
+    if (!destination) {
+      return
+    }
+
+    const items = reorder(todo, source.index, destination.index)
+
+    setTodo(items)
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+    <div className="flex justify-center pt-40">
+      <div className="max-w-sm w-full shadow-lg bg-white p-8 rounded-xl opacity-70">
+        
+        <div className="flex justify-center cursor-default bg-gray-200 rounded-3xl px-4 py-1 color-gray hover:scale-110 transition-all">
+          <img className="object-cover rounded-full w-16 h-16 m-2" src="https://avatars.githubusercontent.com/u/52646391?v=4" alt="codrkai" />
+          <div className="w-full p-3">
+            <p className="text-3xl text-gray-600">Todo List</p>
+            <p className="text-sm">{format(new Date(), 'MMMM d, yyyy')}</p>
+          </div>
         </div>
+
+        <div className="relative mt-10">
+          <div className="absolute inset-y-0 left-2 flex items-center pl-3 pointer-events-none">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-plus-circle" viewBox="0 0 16 16"> <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/> <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/></svg>
+          </div>
+          <input type="text" id="newTodo" value={newTodo} onChange={(e) => setNewTodo(e.target.value)} onKeyUp={(e) => handleKeyUp(e.key)} className="block w-full pl-10 p-2 border-4 rounded-full bg-gray-600 text-white" placeholder="new todo item" />
+        </div>
+
+        <DragDropContext onDragEnd={handleOnDragEnd}>
+          <Droppable droppableId="droppable">
+          {(droppableProvided) => (
+            <div 
+              {...droppableProvided.droppableProps} 
+              ref={droppableProvided.innerRef}
+            >
+              <ul className="block w-full pt-6">
+              {
+                todo?.map( (item, index) => {
+                  return (
+                    <Draggable draggableId={item.id} key={item.id} index={index}>
+                      {(draggableProvided) => (
+                        <div 
+                          {...draggableProvided.draggableProps} 
+                          {...draggableProvided.dragHandleProps} 
+                          ref={draggableProvided.innerRef}
+                        >
+                          <li key={item.id} className="w-full border-2 rounded-xl mt-2 hover:border-blue-300">
+                            <input id={index} type="checkbox" className="float-left block w-6 h-6 m-3" />
+                            <button id={index} onClick={() => handleDelete(index)} className="float-right w-7 h-7 m-2.5 rounded-2xl bg-red-700 text-gray-200 shadow-md hover:bg-red-500 hover:scale-105">x</button>
+                            <label htmlFor={index} className="block w-full p-3">{item.content}</label>
+                          </li>
+                        </div>
+                      )}
+                    </Draggable>
+                  )
+                })
+              }
+              </ul>
+              {droppableProvided.placeholder}
+            </div>
+          )}
+          </Droppable>
+        </DragDropContext>
+
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
+    </>
   )
 }
